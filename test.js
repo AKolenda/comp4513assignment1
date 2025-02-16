568a4a1e713f20fb8096e097da48d9798a702199
AKolenda <akole779@mtroyal.ca>
2025-02-16 02:33:28 -0700

    removed

--- test.js
+++
@@ -1,7 +0,0 @@
-const { data, error } = await supabase.rpc("execute_sql", {
-  query: `
-SELECT orders.id, orders.product_id, users.username
-FROM orders
-INNER JOIN users ON orders.user_id = users.id;
-`,
-});
